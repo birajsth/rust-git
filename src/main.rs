8881af2fcbdc3d10465e4c8093a9c0b211b7d881
brsth <sthbiraj@gmail.com>
2024-06-17 09:28:14 -0700

    initialize .git directory setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::{Parser, Subcommand};
+use std::fs;
+
+/// 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+/// Doc comment
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Doc comment
+    Init,
+}
+
 fn main() {
-    println!("Setting up git repository");
+    let args = Args::parse();
+
+
+    println!("Logs from your program will appear hear!");
+
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+    } 
 }
