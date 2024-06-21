530e2793b0ef7f838810b3c7cf316791b0900798
brsth <sthbiraj@gmail.com>
2024-06-20 19:59:21 -0700

    Implement commit-tree

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,15 @@ enum Command {
         tree_hash: String,
     },
     WriteTree,
+    CommitTree {
+        #[clap(short = 'n')]
+        message: String,
+
+        #[clap(short = 'p')]
+        parent_hash: Option<String>,
+
+        tree_hash: String,
+    },
 }
 
 
@@ -62,6 +71,12 @@ fn main() -> anyhow::Result<()> {
             tree_hash
         } => commands::ls_tree::invoke(name_only, &tree_hash)?,
         Command::WriteTree => commands::write_tree::invoke()?,
+        Command::CommitTree { 
+            message, 
+            tree_hash, 
+            parent_hash
+        } =>  commands::commit_tree::invoke(message , tree_hash, parent_hash)?,
+        
     } 
 
     Ok(())
